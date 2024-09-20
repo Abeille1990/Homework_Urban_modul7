e767073ca32df88593334a538f7f95b5ad7426a1
Abeille1990 <tatiana.tyukova@gmail.com>
2024-09-20 16:02:20 +0300

    Add files via upload

--- modul_7_2.py
+++ modul_7_2.py
@@ -4,15 +4,20 @@ info = ['Text for tell.',
         'Because there are 2 languages!',
         'Спасибо!']
 
-
 def custom_write(file_name, strings):
     file = open(file_name, 'w', encoding='UTF-8')
+    string_positions = {}
     for n in strings:
         b = file.tell()
         file.write(f'{n}\n')
         c = strings.index(n)+1
-        print({f'({c},{b}):{n}'})
+        coordinates = (b,c)
+        string_positions[coordinates ] = n
     file.close()
+    # print(string_positions)
+    return string_positions
 
+result = custom_write('test.txt', info)
 
-custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
